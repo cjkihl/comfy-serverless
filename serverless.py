46e6ec00a9832f05b106fd6ee77c8a350f872938
cjkihl <carljohan.kihl@gmail.com>
2023-12-24 22:01:58 +0800

    updates

--- serverless.py
+++ serverless.py
@@ -1,22 +1,22 @@
-import random
 import time
 from aiohttp import web
-from marshmallow import Schema, fields, ValidationError
+from marshmallow import Schema, fields
 import torch
 
 from nodes import (
-    NODE_CLASS_MAPPINGS,
-    CLIPTextEncode,
-    EmptyLatentImage,
-    CheckpointLoaderSimple,
-    KSampler,
-    SaveImage,
-    VAEDecode,
     init_custom_nodes,
 )
-from sl import img, stats
 from comfy.cli_args import args
-import folder_paths
+from sl import stats
+from sl.cd import (
+    encode_clip,
+    load_loras,
+    load_model,
+    sample,
+    save_images,
+    upscale,
+    validate,
+)
 
 routes = web.RouteTableDef()
 
@@ -38,74 +38,23 @@ class GenerateSchema(Schema):
     batch_size = fields.Int(default=1)
     sampler = fields.Str(default="Euler")
     test = fields.Bool(default=False)
+    loras = fields.List(fields.Str(), default=[])
 
 
 @routes.post("/v1/generate")
 async def text2img(request):
     start_time = time.time()
 
-    data = await request.json()
-    print(data)
-    schema = GenerateSchema()
-    try:
-        d = schema.dump(schema.load(data))
-    except ValidationError as err:
-        print(err.messages)
-        return web.json_response({"error": True, "message": err.messages}, status=400)
+    (d, r) = await validate(GenerateSchema, request)
+    if r is not None:
+        return r
 
-    if d["test"] is True:
-        return web.json_response({"time": 0.0, "img": "", **d})
-
-    # Load model
-    checkpoint_loader = CheckpointLoaderSimple()
-    checkpoint_name = folder_paths.get_filename_list("checkpoints")[0]
-    print(checkpoint_name)
-    model, clip, vae = checkpoint_loader.load_checkpoint(checkpoint_name)
-    print("Checkpoint Loaded")
-
-    # CLIP Text encoder
-    with torch.no_grad():
-        clip_encoder = CLIPTextEncode()
-        (positive,) = clip_encoder.encode(clip, d["prompt"])
-        (negative,) = clip_encoder.encode(clip, d["negative_prompt"])
-        print("CLIP Encoded")
-
-    n = EmptyLatentImage()
-    (latent,) = n.generate(
-        width=d["width"], height=d["height"], batch_size=d["batch_size"]
-    )
-    print("Latent Image Generated")
-
-    if d["seed"] == -1:
-        d["seed"] = random.randint(0, 0xFFFFFFFFFFFFFFFF)
-
-    s = KSampler()
-    (samples,) = s.sample(
-        model,
-        seed=d["seed"],
-        steps=d["steps"],
-        cfg=d["cfg_scale"],
-        sampler_name=d["sampler"],
-        scheduler="normal",
-        positive=positive,
-        negative=negative,
-        latent_image=latent,
-        denoise=1.0,
-    )
-    print("Samples Generated")
-
-    decoder = VAEDecode()
-    (decoded,) = decoder.decode(vae, samples)
-    print("VAE decoded")
-
-    with torch.no_grad():
-        img_saver = SaveImage()
-        img_saver.save_images(decoded, filename_prefix="CD")
-
-    print("Images Saved")
-
-    images = img.images_to_base64(decoded)
-    print("Images Base 64 Encoded")
+    with torch.inference_mode():
+        model, clip, vae = load_model()
+        model, clip = load_loras(d["loras"], model, clip)
+        (positive, negative) = encode_clip(clip, d)
+        decoded = sample(model, d, positive, negative, vae)
+        images = save_images(decoded)
 
     end_time = time.time()
 
@@ -126,79 +75,22 @@ class UpscaleSchema(Schema):
     denoising_strength = fields.Float(default=0.3)
     img = fields.Str(required=True)
     test = fields.Bool(default=False)
+    loras = fields.List(fields.Str(), default=[])
 
 
 @routes.post("/v1/upscale")
 async def img2img(request):
     start_time = time.time()
-    data = await request.json()
-    schema = UpscaleSchema()
+    (d, r) = await validate(UpscaleSchema, request)
+    if r is not None:
+        return r
 
-    try:
-        d = schema.load(data)
-    except ValidationError as err:
-        print(err.messages)
-        return web.json_response({"error": True, "message": err.messages}, status=400)
-
-    if d["test"] is True:
-        return web.json_response({"time": 0.0, "img": "", **schema.dump(d)})
-
-    # Load model
-    checkpoint_loader = CheckpointLoaderSimple()
-    checkpoint_name = folder_paths.get_filename_list("checkpoints")[0]
-    print(checkpoint_name)
-    model, clip, vae = checkpoint_loader.load_checkpoint(checkpoint_name)
-    print("Checkpoint Loaded")
-
-    # Load Upscaler
-    class_def = NODE_CLASS_MAPPINGS["UpscaleModelLoader"]
-    obj = class_def()
-    upscale_name = folder_paths.get_filename_list("upscale_models")[0]
-    (upscale_model,) = getattr(obj, class_def.FUNCTION)(upscale_name)
-
-    # CLIP Text encoder
-    clip_encoder = CLIPTextEncode()
-    (positive,) = clip_encoder.encode(clip, d["prompt"])
-    (negative,) = clip_encoder.encode(clip, d["negative_prompt"])
-
-    (latent, _mask) = img.base64_to_image(d["img"])
-
-    class_def = NODE_CLASS_MAPPINGS["UltimateSDUpscale"]
-    obj = class_def()
-    (upscaled,) = getattr(obj, class_def.FUNCTION)(
-        image=latent,
-        model=model,
-        upscale_model=upscale_model,
-        positive=positive,
-        negative=negative,
-        vae=vae,
-        upscale_by=d["upscale_by"],
-        seed=d["seed"],
-        steps=d["steps"],
-        cfg=d["cfg_scale"],
-        sampler_name=d["sampler"],
-        scheduler="normal",
-        denoise=0.1,
-        mode_type="Linear",
-        tile_width=512,
-        tile_height=512,
-        mask_blur=8,
-        tile_padding=32,
-        seam_fix_mode="None",
-        seam_fix_denoise=0.0,
-        seam_fix_mask_blur=0.0,
-        seam_fix_width=0,
-        seam_fix_padding=0,
-        force_uniform_tiles=False,
-        tiled_decode=False,
-    )
-
-    img.save_image(
-        upscaled,
-        filename_prefix="CD",
-    )
-
-    images = img.images_to_base64(upscaled)
+    with torch.inference_mode():
+        (model, clip, vae) = load_model()
+        model, clip = load_loras(d["loras"], model, clip)
+        (positive, negative) = encode_clip(clip, d)
+        upscaled = upscale(model, d, positive, negative, vae)
+        images = save_images(upscaled)
 
     end_time = time.time()
 
--- sl/cd.py
+++ sl/cd.py
@@ -1,122 +1,165 @@
+from ast import Dict
 import random
-import torch
+from typing import List
+from marshmallow import ValidationError
+from aiohttp import web
+from torch import Tensor
+from comfy.sd import load_lora_for_models
+from comfy.utils import load_torch_file
 
-import comfy.sd
-import comfy.sample
-import comfy.model_management
-import latent_preview
-
-
-def text2img_sampler(
-    model,
-    positive,
-    negative,
-    latent: torch.Tensor,
-    seed: int = -1,
-    steps: int = 3,
-    cfg: int = 7,
-    sampler_name: str = "euler",
-    scheduler: str = "normal",
-    denoise=1.0,
-):
-    if seed == -1:
-        seed = random.randint(0, 0xFFFFFFFFFFFFFFFF)
-
-    out = ksampler(
-        model,
-        seed,
-        steps,
-        cfg,
-        sampler_name,
-        scheduler,
-        positive,
-        negative,
-        latent,
-        denoise,
-        disable_noise=False,
-    )
-    print("Sampling done")
-    return out["samples"]
+from nodes import (
+    NODE_CLASS_MAPPINGS,
+    CLIPTextEncode,
+    CheckpointLoaderSimple,
+    EmptyLatentImage,
+    KSampler,
+    SaveImage,
+    VAEDecode,
+)
+from sl import img
+import folder_paths
 
 
-def ksampler(
-    model,
-    seed,
-    steps,
-    cfg,
-    sampler_name,
-    scheduler,
-    positive,
-    negative,
-    latent,
-    denoise=1.0,
-    disable_noise=False,
-    start_step=None,
-    last_step=None,
-    force_full_denoise=False,
-):
-    latent_image = latent["samples"]
-    if disable_noise:
-        noise = torch.zeros(
-            latent_image.size(),
-            dtype=latent_image.dtype,
-            layout=latent_image.layout,
-            device="cpu",
+nodes = []
+
+
+async def validate(schema_def, request):
+    data = await request.json()
+    schema = schema_def()
+    print({"input_data": data})
+    try:
+        d = schema.dump(schema.load(data))
+        if d["test"] is True:
+            return (None, web.json_response(d))
+    except ValidationError as err:
+        print(err.messages)
+        return (
+            None,
+            web.json_response({"error": True, "message": err.messages}, status=400),
         )
-    else:
-        batch_inds = latent["batch_index"] if "batch_index" in latent else None
-        noise = comfy.sample.prepare_noise(latent_image, seed, batch_inds)
+    return (d, None)
 
-    noise_mask = None
-    if "noise_mask" in latent:
-        noise_mask = latent["noise_mask"]
 
-    callback = prepare_callback(model, steps)
-    disable_pbar = True
-    samples = comfy.sample.sample(
+def load_model():
+    checkpoint_loader = CheckpointLoaderSimple()
+    l = folder_paths.get_filename_list("checkpoints")
+    if not l:
+        raise FileNotFoundError("No checkpoints found")
+    n = l[0]
+    print(n)
+    model, clip, vae = checkpoint_loader.load_checkpoint(n)
+    print("Checkpoint Loaded")
+    return (model, clip, vae)
+
+
+# Lora cache
+loaded_loras: Dict[str, Dict[str, Tensor]] = {}
+
+
+def load_loras(names: List[str] | None, model, clip):
+    for n in names:
+        lora_name, strength = (n.split(":") + [1])[:2]
+        lora = loaded_loras.get(lora_name)
+        if lora is None:
+            lora_path = folder_paths.get_full_path("loras", lora_name)
+            print("Loading lora", lora_path)
+            loaded_loras[lora_name] = lora = load_torch_file(lora_path, safe_load=True)
+        model, clip = load_lora_for_models(model, clip, lora, strength, strength)
+    return (model, clip)
+
+
+def load_upscaler():
+    class_def = NODE_CLASS_MAPPINGS["UpscaleModelLoader"]
+    obj = class_def()
+    l = folder_paths.get_filename_list("upscale_models")
+    if not l:
+        raise FileNotFoundError("No upscale models found")
+    n = l[0]
+    print(n)
+    (upscale_model,) = getattr(obj, class_def.FUNCTION)(n)
+    print("Upscale Model Loaded")
+    return (upscale_model,)
+
+
+def encode_clip(clip, d):
+    print("Encoding CLIP")
+    clip_encoder = CLIPTextEncode()
+    (positive,) = clip_encoder.encode(clip, d["prompt"])
+    (negative,) = clip_encoder.encode(clip, d["negative_prompt"])
+    print("CLIP Encoded")
+    return (positive, negative)
+
+
+def save_images(images):
+    img_saver = SaveImage()
+    img_saver.save_images(images, filename_prefix="CD")
+    print("Images Saved")
+    images = img.images_to_base64(images)
+    print("Images Base 64 Encoded")
+    return images
+
+
+def sample(model, d, positive, negative, vae):
+    n = EmptyLatentImage()
+    (latent,) = n.generate(
+        width=d["width"], height=d["height"], batch_size=d["batch_size"]
+    )
+    print("Latent Image Generated")
+    if d["seed"] == -1:
+        d["seed"] = random.randint(0, 0xFFFFFFFFFFFFFFFF)
+
+    s = KSampler()
+    (samples,) = s.sample(
         model,
-        noise,
-        steps,
-        cfg,
-        sampler_name,
-        scheduler,
-        positive,
-        negative,
-        latent_image,
-        denoise=denoise,
-        disable_noise=disable_noise,
-        start_step=start_step,
-        last_step=last_step,
-        force_full_denoise=force_full_denoise,
-        noise_mask=noise_mask,
-        callback=callback,
-        disable_pbar=disable_pbar,
-        seed=seed,
+        seed=d["seed"],
+        steps=d["steps"],
+        cfg=d["cfg_scale"],
+        sampler_name=d["sampler"],
+        scheduler="normal",
+        positive=positive,
+        negative=negative,
+        latent_image=latent,
+        denoise=1.0,
     )
-    out = latent.copy()
-    out["samples"] = samples
-    return out
+    print("Samples Generated")
+    decoder = VAEDecode()
+    (decoded,) = decoder.decode(vae, samples)
+    print("VAE decoded")
+    return decoded
 
 
-def prepare_callback(model, steps, x0_output_dict=None):
-    """Prepare Callback"""
-    preview_format = "JPEG"
-    if preview_format not in ["JPEG", "PNG"]:
-        preview_format = "JPEG"
-
-    previewer = latent_preview.get_previewer(
-        model.load_device, model.model.latent_format
+def upscale(model, d, positive, negative, vae):
+    (upscale_model,) = load_upscaler()
+    (latent, _) = img.base64_to_image(d["img"])
+    print("Start Upscaling")
+    class_def = NODE_CLASS_MAPPINGS["UltimateSDUpscale"]
+    obj = class_def()
+    (upscaled,) = getattr(obj, class_def.FUNCTION)(
+        image=latent,
+        model=model,
+        upscale_model=upscale_model,
+        positive=positive,
+        negative=negative,
+        vae=vae,
+        upscale_by=d["upscale_by"],
+        seed=d["seed"],
+        steps=d["steps"],
+        cfg=d["cfg_scale"],
+        sampler_name=d["sampler"],
+        scheduler="normal",
+        denoise=0.1,
+        mode_type="Linear",
+        tile_width=512,
+        tile_height=512,
+        mask_blur=8,
+        tile_padding=32,
+        seam_fix_mode="None",
+        seam_fix_denoise=0.0,
+        seam_fix_mask_blur=0.0,
+        seam_fix_width=0,
+        seam_fix_padding=0,
+        force_uniform_tiles=False,
+        tiled_decode=False,
     )
-
-    print(steps)
-
-    def callback(step, x0, x, total_steps):
-        if x0_output_dict is not None:
-            x0_output_dict["x0"] = x0
-
-        preview_bytes = None
-        if previewer:
-            preview_bytes = previewer.decode_latent_to_preview_image(preview_format, x0)
-        print(step + 1, total_steps, preview_bytes)
-
-    return callback
+    print("Upscaling done")
+    return upscaled
